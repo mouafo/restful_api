adb188ac9632ce22b0a4791a772a01b1e88b988d
jbouhier <jb.bouhier@gmail.com>
2015-06-18 11:36:53 +0200

    Added user(s)/{id} routes

--- web/index.php
+++ web/index.php
@@ -7,10 +7,19 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 // ... definitions
+// MariaDB > user:rest, passwd:happy little api
 
 // Routes
 $app->get('/', function () {
     return false;
+});
+
+$app->get('/users/{id}', function (Silex\Application $app, $id) {
+    return 'Route: /users/' . [$id];
+});
+
+$app->get('/user/{id}', function (Silex\Application $app, $id) {
+    return 'Route: /user/' . [$id];
 });
 
 // Start application on the last line
