88933390847ad6b42ffc000f7ace92bece194ab2
jbouhier <jb.bouhier@gmail.com>
2015-06-18 13:52:59 +0200

    Fix index.php: route return Response object

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 // web/index.php
@@ -12,10 +12,9 @@ $app['debug'] = true;
 // ... definitions
 // MariaDB > user:rest, passwd:happy little api
 
-
 // Routes
 $app->get('/', function () {
-    return new Response(200);
+    return new Response('', 200);
 });
 
 $app->get('/users/{id}', function (Silex\Application $app, $id) {
@@ -24,6 +23,14 @@ $app->get('/users/{id}', function (Silex\Application $app, $id) {
 
 $app->get('/user/{id}', function (Silex\Application $app, $id) {
     return 'Route: /user/' . [$id];
+});
+
+// POST exemple
+$app->post('/post/form', function (Request $request) {
+    $message = $request->get('message');
+    mail('feedback@yoursite.com', '[YourSite] Feedback', $message);
+
+    return new Response('Thank you for your feedback!', 201);
 });
 
 
---
+++ web/notes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * HTTP methods
+ *
+ * $app->get('/some/route/{id}', function () {
+ *      // ....
+ * });
+ *
+ *
+ *  * $app->post('/some/route/{id}', function () {
+ *      // ....
+ * });
+ *
+ *  * $app->put('/some/route/{id}', function () {
+ *      // ....
+ * });
+ *
+ *  * $app->delete('/some/route/{id}', function () {
+ *      // ....
+ * });
+ */
