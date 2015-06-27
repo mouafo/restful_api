bd4b075e1a0f7e4ad157d284ebb27a1f5c9ee02b
jbouhier <jb.bouhier@gmail.com>
2015-06-28 00:50:46 +0200

    Refactored duplicate code

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -25,32 +26,33 @@ $app->get('/', function () use ($app) {
     return new Response('', 200);
 });
 
-$app->get('/users/{id}', function ($id) use ($app) {
-    $query = "SELECT * FROM user WHERE id = $id";
-    $user = $app['db']->fetchAssoc($query, array((int) $id));
+
+$app->get('/user/{id}', function ($id) use ($app) {
+    $query = 'SELECT * FROM user WHERE id = :id';
+    $user = $app['db']->fetchAssoc($query, array('id'=> (int) $id));
 
     if ($user == false)
         return $app->json(array('status' => 404, 'message' => 'Not Found'));
     if ($user['role'] == 'admin')
         return $app->json(array('status' => 401, 'message' => 'Unauthorized'));
 
-
-    return $app->json($user);
-})->assert('id', '\d+');
-
-$app->get('/user/{id}', function ($id) use ($app) {
-    $query = "SELECT * FROM user WHERE id = $id";
-    $user = $app['db']->fetchAssoc($query, array((int) $id));
-
-    if ($user == false)
-        return new Response('', 404);
-    if ($user['role'] == 'admin')
-        return new Response('', 401);
-
     return $app->json($user);
 })->assert('id', '\d+');
 
 
+$app->get('/users/{id}', function ($id) use ($app) {
+    return new RedirectResponse('/user/'.$id);
+})->assert('id', '\d+');
+
+
+$app->get('/search/users', function (Request $request) use ($app) {
+    $email = $request->get('q');
+//    die($email);
+    $query = "SELECT * FROM user u WHERE u.email = $email";
+    $user = $app['db']->fetchAssoc($query, array($email));
+
+    return array($app->json($user));
+});
 
 
 
